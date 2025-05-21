8587562a3e921939192d9d628cbb6dbbe94e121b
lolpop1228 <87169704+lolpop1228@users.noreply.github.com>
2025-05-21 21:29:31 +0700

    im sorry

--- models/m4_EN2TH.py
+++ models/m4_EN2TH.py
@@ -1,13 +1,27 @@
-from transformers import MarianMTModel, MarianTokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 class ENtoTHTranslation:
     def __init__(self):
-        self.model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-en-th")
-        self.tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-th")
+        model_name = "facebook/nllb-200-distilled-600M"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.src_lang = "eng_Latn"
+        self.tgt_lang = "tha_Thai"
 
     def translate(self, text):
-        translated = self.model.generate(**self.tokenizer(text, return_tensors="pt", padding=True))
-        return self.tokenizer.decode(translated[0], skip_special_tokens=True)
+        # Set source language in tokenizer
+        self.tokenizer.src_lang = self.src_lang
+
+        # Tokenize with source language
+        encoded = self.tokenizer(text, return_tensors="pt", padding=True)
+
+        # Generate with target language ID
+        generated = self.model.generate(
+            **encoded,
+            forced_bos_token_id=self.tokenizer.convert_tokens_to_ids(self.tgt_lang)
+        )
+
+        return self.tokenizer.decode(generated[0], skip_special_tokens=True)
 
 # Singleton instance
 _en2th_instance = None
--- models/m5_TH2EN.py
+++ models/m5_TH2EN.py
@@ -1,13 +1,27 @@
-from transformers import MarianMTModel, MarianTokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 class THtoENTranslation:
     def __init__(self):
-        self.model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-th-en")
-        self.tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-th-en")
+        model_name = "facebook/nllb-200-distilled-600M"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.src_lang = "tha_Thai"
+        self.tgt_lang = "eng_Latn"
 
     def translate(self, text):
-        translated = self.model.generate(**self.tokenizer(text, return_tensors="pt", padding=True))
-        return self.tokenizer.decode(translated[0], skip_special_tokens=True)
+        # Set source language in tokenizer
+        self.tokenizer.src_lang = self.src_lang
+
+        # Tokenize with source language
+        encoded = self.tokenizer(text, return_tensors="pt", padding=True)
+
+        # Generate with target language ID
+        generated = self.model.generate(
+            **encoded,
+            forced_bos_token_id=self.tokenizer.convert_tokens_to_ids(self.tgt_lang)
+        )
+
+        return self.tokenizer.decode(generated[0], skip_special_tokens=True)
 
 # Singleton instance
 _th2en_instance = None
